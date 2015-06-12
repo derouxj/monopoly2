fe34e58dbc1e2743766df90ed6db2c27b723d558
derouxj <derouxjulien@gmail.com>
2015-06-12 10:46:22 +0200

    Merge branch 'master' of https://github.com/derouxj/monopoly2
    Conflicts:
    	save.db
    	score.db
    	src/Jeu/CarreauTirage.java
    	src/Jeu/CouleurPropriete.java
    	src/Jeu/Monopoly.java
    	src/Jeu/jeu.java
    	src/UI/Interface.java

--- src/Jeu/CarreauTirage.java
+++ src/Jeu/CarreauTirage.java
@@ -3,24 +3,22 @@ package Jeu;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
-public class CarreauTirage extends CarreauAction implements java.io.Serializable{
-    
+public class CarreauTirage extends CarreauAction implements java.io.Serializable {
 
-
-    public CarreauTirage(int numero, String nomCarreau,Monopoly monopoly) {
-        super(numero, nomCarreau,monopoly);
+    public CarreauTirage(int numero, String nomCarreau, Monopoly monopoly) {
+        super(numero, nomCarreau, monopoly);
     }
 
     @Override
     public void action(Joueur j) {
         Carte ct;
-        if(super.getNomCarreau().equals("Chance")) {
+        if (super.getNomCarreau().equals("Chance")) {
             ct = super.getMonopoly().tirerCarteChance();
         } else {
             ct = super.getMonopoly().tirerCarteCaisseCommunaute();
         }
-        
-        String monType=ct.getType();
+
+        String monType = ct.getType();
         if (monType.equals("L")) {
             System.out.println(ct.getDescription());
             j.ajouterCartePrison();
@@ -33,10 +31,10 @@ public class CarreauTirage extends CarreauAction implements java.io.Serializable
             this.anniversaire(j);
         } else if (monType.equals("A")) {
             System.out.println(ct.getDescription());
-            if (ct.getNombreAction()>0) {
+            if (ct.getNombreAction() > 0) {
                 j.recevoir(ct.getNombreAction());
             } else {
-                j.payer(ct.getNombreAction()*-1);
+                j.payer(ct.getNombreAction() * -1);
             }
         } else if (monType.equals("B")) {
             System.out.println(ct.getDescription());
@@ -47,12 +45,12 @@ public class CarreauTirage extends CarreauAction implements java.io.Serializable
             j.envoyerPrison();
         } else if (monType.equals("M")) {
             System.out.println(ct.getDescription());
-            this.reparation(j,ct);
+            this.reparation(j, ct);
         } else {
             System.out.println("ERREUR, type non reconnu");
         }
-    } 
-    
+    }
+
     public void anniversaire(Joueur j) {
         LinkedList<Joueur> collectJoueurs = new LinkedList<Joueur>(super.getMonopoly().getJoueurs());
         //collectJoueurs=super.getMonopoly().getJoueurs();
@@ -60,9 +58,10 @@ public class CarreauTirage extends CarreauAction implements java.io.Serializable
         for (Joueur js : collectJoueurs) {
             js.payer(10);
         }
-        j.recevoir(10*collectJoueurs.size());
+        j.recevoir(10 * collectJoueurs.size());
     }
-    public void reparation(Joueur j,Carte ct) {
+
+    public void reparation(Joueur j, Carte ct) {
         int nbM = 0;
         int nbH = 0;
         int prixR;
@@ -74,8 +73,8 @@ public class CarreauTirage extends CarreauAction implements java.io.Serializable
         }
         prixR = nbH * ct.getReparationHotel() + nbM * ct.getReparationMaison();
         j.payer(prixR);
-        
-            System.out.println("Les réparations ont été faites sur " + nbM +" maisons et " + nbH + " hotels pour le prix de "+ prixR + "$.");
+
+        System.out.println("Les réparations ont été faites sur " + nbM + " maisons et " + nbH + " hotels pour le prix de " + prixR + "$.");
     }
 
 }
